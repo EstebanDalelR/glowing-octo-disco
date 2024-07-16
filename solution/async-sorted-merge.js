b83eab20b80d43e52b32de5bcff3794fba5a6d4b
Esteban Dalel R <esteban@astronuts.io>
2024-07-16 18:38:42 -0500

    Explain solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -3,6 +3,10 @@
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  console.log(logSources.length);
+  // Similar to the sync solution, I will pop the first entry from each logSource and compare them
+  // the difference is that I will have to wait for a new entry if the random so decides it
+
   return new Promise((resolve, reject) => {
     resolve(console.log("Async sort complete."));
   });
