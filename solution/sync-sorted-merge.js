827d6d4b9b98707e1ab03a91761a1ed67302d5fe
Esteban Dalel R <esteban@astronuts.io>
2024-07-16 18:31:58 -0500

    Code solution

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -10,5 +10,27 @@ module.exports = (logSources, printer) => {
   // then, I will print the earliest and replace it with the next entry from the same source
   // I will repeat the process until all entries are printed from all sources
 
+  let logs = logSources.map((logSource) => logSource.pop());
+  let sources = logs.map((log, index) => index);
+
+  let minIndex = sources.reduce((minIndex, source) => {
+    if (logs[source] && logs[source].date < logs[minIndex].date) {
+      return source;
+    }
+    return minIndex;
+  }, 0);
+
+  while (logs[minIndex]) {
+    printer.print(logs[minIndex]);
+    logs[minIndex] = logSources[minIndex].pop();
+    minIndex = sources.reduce((minIndex, source) => {
+      if (logs[source] && logs[source].date < logs[minIndex].date) {
+        return source;
+      }
+      return minIndex;
+    }, 0);
+  }
+  printer.done();
+
   return console.log("Sync sort complete.");
 };
