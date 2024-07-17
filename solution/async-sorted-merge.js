1fe5fc54de14b406da8205bc891b0cbf74fdd363
Esteban Dalel R <esteban@astronuts.io>
2024-07-16 20:36:49 -0500

    Add helper functions

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -9,6 +9,31 @@ module.exports = (logSources, printer) => {
   // When adding to the queue, I will resolve the promise and add the log entry to the sorted queue
   // might be added at any place in the queue
   // wait, did I reinvent kafka? lol
+
+  let queue = [];
+
+  // helper async function to get the next log entry from a log source
+  // will give the log or false if the log source has ended
+  const popNext = async (index) => {
+    return logSources[index].popAsync();
+  };
+
+  // Add the log entry to the queue in the correct order
+  const addToQueue = (sourceIndex, log) => {
+    const logEntry = { sourceIndex, log };
+    if (queue.length === 0) {
+      queue.push(logEntry);
+    } else {
+      let insertIndex = queue.findIndex((entry) => entry.log.date > log.date);
+      // not found, bigger than the last one
+      if (insertIndex === -1) {
+        queue.push(logEntry);
+      } else {
+        queue.splice(insertIndex, 0, logEntry);
+      }
+    }
+  };
+
   return new Promise((resolve, reject) => {
     resolve(console.log("Async sort complete."));
   });
